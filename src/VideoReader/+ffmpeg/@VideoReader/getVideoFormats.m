133b145758c68f3ba42a2857cde625411425edea
Kesh Ikuma <hokiedsp@users.noreply.github.com>
2017-12-08 10:16:15 -0600

    Updated MATLAB help text

--- src/VideoReader/+ffmpeg/@VideoReader/getVideoFormats.m
+++ src/VideoReader/+ffmpeg/@VideoReader/getVideoFormats.m
@@ -1,5 +1,5 @@
 function formats = getVideoFormats()
-%FFMPEG.GETVIDEOFORMATS   Get supported video pixel formats
+%FFMPEG.VIDEOREADER.GETVIDEOFORMATS   Get supported video pixel formats
 %   FORMATS = FFMPEG.VIDEOREADER.GETVIDEOFORMATS() returns a struct
 %   array of supported video formats (FFmpeg's PixelFormat). Any of the
 %   listed format could be used for a ffmpeg.VideoReader object by
@@ -8,37 +8,25 @@ function formats = getVideoFormats()
 %   The fields of the returned struct array are:
 %
 %   Name               - Name of the PixelFormat
-%   Alias
+%   Alias              - Other accepted name of the format
 %   NumberOfComponents - Number of Video Components (frames' 3rd dimension)
-%   BitsPerPixel
-%   RGB
-%   Alpha
-%   Palletted
-%   HWAccel
-%   Bayer
-%   Log2ChromaW
-%   Log2ChromaH
+%   BitsPerPixel       - Number of bits to represent one bit
+%   RGB                - 'on' if contains RGB-like data; 'off' if YUV/grayscale
+%   Alpha              - 'on' if supports alpha channel
+%   Paletted           - 'on' if a color palet is also given, 'psudo' if uses a fixed palette
+%   HWAccel            - 'on' if HW accelerated
+%   Bayer              - 'on' if follows Bayer pattern
+%   Log2ChromaW        - Amount to shift the luma width right to find the chroma width
+%   Log2ChromaH        - Amount to shift the luma height right to find the chroma height
 %   
-%   hold video data ContainsAudio - The File Format can hold audio data
+%   See also: ffmpeg.VideoReader.getFileFormats,
+%             ffmpeg.VideoReader.getVideoCompressions
+
 
 ffmpegsetenv();
 formats = ffmpeg.VideoReader.mex_backend([], 'static', mfilename);
 
 if nargout==0
-%    fnames = fieldnames(formats);
-%    formats = rmfield(formats,setdiff(fnames,{'Name','RGB','BitsPerPixel','Alpha'}));
    display(struct2table(formats));
    clear formats
 end
-% extensions = audiovideo.mmreader.getSupportedFormats();
-% formats = audiovideo.FileFormatInfo.empty();
-% for ii=1:length(extensions)
-%    formats(ii) = audiovideo.FileFormatInfo( extensions{ii}, ...
-%       VideoReader.translateDescToLocale(extensions{ii}), ...
-%       true, ...
-%       false );
-% end
-% 
-% % sort file extension
-% [~, sortedIndex] = sort({formats.Extension});
-% formats = formats(sortedIndex);
