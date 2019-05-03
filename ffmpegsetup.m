46a884a880e89cbd78218fa999f7dfab78f8f9f7
Takeshi Ikuma <tikuma@gmail.com>
2019-05-02 23:40:58 -0500

    Direct downloading of ffmpeg binaries

--- ffmpegsetup.m
+++ ffmpegsetup.m
@@ -7,56 +7,61 @@ function ffmpegsetup
 % Copyright 2013 Takeshi Ikuma
 % History:
 % rev. - : (06-19-2013) original release
+% rev. 1 : (05-01-2019) auto-download ffmpeg
 
 % dlgtitle = 'FFMPEG Toolbox Setup';
 % ffmpegdir = fileparts(which(mfilename));
 
-% get existing config
-if ispref('ffmpeg','exepath')
-   ffmpegexe = getpref('ffmpeg','exepath');
-else
-   ffmpegexe = '';
-end
-
-% first check if it is arleady in the system path (should be the case for
-% both linux & mac
-ffmpegnames = 'ffmpeg';
-[fail,~] = system([ffmpegnames ' -v']);
-if ~fail
-   ffmpegexe = ffmpegnames;
-   setpref('ffmpeg','exepath',ffmpegexe); % save for later
-   return;
-end
-
-% if not found, ask user for the location of the file
-switch lower(computer)
-   case {'pcwin' 'pcwin64'}
-      filter = {'ffmpeg.exe','FFMPEG EXE file';'*.exe','All EXE files (*.exe)'};
-   otherwise % linux/mac
-      filter = {'*','All files'};
-end
-
-[filename, pathname] = uigetfile(filter,'Locate FFMPEG executable file',ffmpegexe);
-if numel(filename)>0 && filename(1)~=0 % cancelled
-   ffmpegexe = fullfile(pathname,filename);
-   if any(ffmpegexe==' '), ffmpegexe = ['"' ffmpegexe '"']; end
-   
-   % try
-   [fail,msg] = system([ffmpegexe ' -version']);
-   if fail || isempty(regexp(msg,'^ffmpeg','once'))
+try
+   ffmpegdownload();
+catch
+   % get existing config
+   if ispref('ffmpeg','exepath')
+      ffmpegexe = getpref('ffmpeg','exepath');
+   else
       ffmpegexe = '';
-      disp('Invalid FFMPEG executable specified.');
    end
    
-   setpref('ffmpeg','exepath',ffmpegexe);
+   % first check if it is arleady in the system path (should be the case for
+   % both linux & mac
+   ffmpegnames = 'ffmpeg';
+   [fail,~] = system([ffmpegnames ' -v']);
+   if ~fail
+      ffmpegexe = ffmpegnames;
+      setpref('ffmpeg','exepath',ffmpegexe); % save for later
+      return;
+   end
+   
+   % if not found, ask user for the location of the file
+   switch lower(computer)
+      case {'pcwin' 'pcwin64'}
+         filter = {'ffmpeg.exe','FFMPEG EXE file';'*.exe','All EXE files (*.exe)'};
+      otherwise % linux/mac
+         filter = {'*','All files'};
+   end
+   
+   [filename, pathname] = uigetfile(filter,'Locate FFMPEG executable file',ffmpegexe);
+   if numel(filename)>0 && filename(1)~=0 % cancelled
+      ffmpegexe = fullfile(pathname,filename);
+      if any(ffmpegexe==' '), ffmpegexe = ['"' ffmpegexe '"']; end
+      
+      % try
+      [fail,msg] = system([ffmpegexe ' -version']);
+      if fail || isempty(regexp(msg,'^ffmpeg','once'))
+         ffmpegexe = '';
+         disp('Invalid FFMPEG executable specified.');
+      end
+      
+      setpref('ffmpeg','exepath',ffmpegexe);
+   end
+   
+   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+   
+   ffmpeg = getpref('ffmpeg','exepath');
+   if isempty(ffmpeg)
+      error('FFMPEG executable not found.');
+   else
+      fprintf('   FFMPEG executable: %s\n',ffmpeg);
+   end
+   disp('   ...done.');
 end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-ffmpeg = getpref('ffmpeg','exepath');
-if isempty(ffmpeg)
-   error('FFMPEG executable not found.');
-else
-   fprintf('   FFMPEG executable: %s\n',ffmpeg);
-end
-disp('   ...done.');
