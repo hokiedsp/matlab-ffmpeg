fb9bad422976aafb3d8b18010ac65973ba216a33
Takeshi Ikuma <tikuma@gmail.com>
2019-05-03 09:05:15 -0500

    Added initialization lines

--- ffmpegmediatypes.cpp
+++ ffmpegmediatypes.cpp
@@ -2,6 +2,15 @@
 
 #include <mex.h>
 
+extern "C"
+{
+#include <libavformat/avformat.h>
+#if CONFIG_AVDEVICE
+#include <libavdevice/avdevice.h>
+#endif
+}
+
+#include "ffmpeg/avexception.h"
 #include "ffmpeg/FFmpegInputFile.h"
 
 #define mxAutoCleanUp(p) std::unique_ptr<void, decltype(&mxFree)> cleanup_##p(p, &mxFree) // auto-deallocate the buffer
@@ -13,6 +22,15 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
         mexErrMsgTxt("Takes exactly 1 input argument and produces 1 output.");
     if (!mxIsChar(prhs[0]))
         mexErrMsgTxt("Filename must be given as a character array.");
+
+    // initialize FFmpeg
+    avformat_network_init();
+#if CONFIG_AVDEVICE
+    avdevice_register_all();
+#endif
+
+    // initialize AVException
+    AVException::initialize();
 
     char *filename = mxArrayToUTF8String(prhs[0]);
     mxAutoCleanUp(filename);
