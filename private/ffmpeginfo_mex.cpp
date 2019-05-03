227ca23d94c4c0d1d94d7ddc69e4cb02c6d2f1f1
Takeshi Ikuma <tikuma@gmail.com>
2019-05-03 14:09:38 -0500

    First working state

--- private/ffmpeginfo_mex.cpp
+++ private/ffmpeginfo_mex.cpp
@@ -2,22 +2,43 @@
 
 #include <mex.h>
 
-#include "ffmpeg/FFmpegInputFile.h"
+extern "C"
+{
+#include <libavformat/avformat.h>
+#if CONFIG_AVDEVICE
+#include <libavdevice/avdevice.h>
+#endif
+}
 
-#define mxAutoCleanUp(p) std::unique_ptr<void, decltype(&mxFree)> cleanup_##p(p, &mxFree) // auto-deallocate the buffer
+#include "ffmpeg/FFmpegInputFile.h"
+#include "ffmpeg/avexception.h"
+#include "ffmpeg/ffmpeg_utils.h"
 
 // info = ffmpeginfo_mex(filename)
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-    if (nlhs > 0 || nrhs != 1)
+    if (nlhs > 1 || nrhs != 1)
         mexErrMsgTxt("Takes exactly 1 input argument and produces 1 output.");
     if (!mxIsChar(prhs[0]))
         mexErrMsgTxt("Filename must be given as a character array.");
 
-    char *filename = mxArrayToUTF8String(prhs[0]);
-    mxAutoCleanUp(filename);
+    // initialize FFmpeg
+    avformat_network_init();
+#if CONFIG_AVDEVICE
+    avdevice_register_all();
+#endif
 
+    // initialize AVException
+    AVException::initialize();
+    
+    // get media file name
+    char *filename = mxArrayToUTF8String(prhs[0]);
+    mxAutoFree(filename);
+
+    // open the media file
     FFmpegInputFile mediafile(filename);
+
+    // get a dump of the media info
     plhs[0] = FFmpegInputFile::createMxInfoStruct();
     mediafile.dumpToMatlab(plhs[0]);
 }
