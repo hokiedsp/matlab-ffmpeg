d4120349ebf968081d69e119027296d3a1a8573e
Takeshi Ikuma <tikuma@gmail.com>
2019-05-03 11:06:28 -0500

    Initial commit

---
+++ ffmpegmediatypes.m
@@ -0,0 +1,13 @@
+function types = ffmpegmediatypes(infile)
+%FFMPEGMEDIATYPES   Retrieves list of streams available in the media file
+%   TYPES = FFMPEGMEDIATYPES(FILE) returns TYPES cell-string array containing 
+%   the list of stream types found in the media file specified by the string 
+%   FILE.
+%
+%   See Also: FFMPEGMEDIATYPES
+
+% Copyright 2019 Takeshi Ikuma
+% History:
+% rev. - : (05-03-2019) original release
+
+% Documentation m-file for ffmpegmediatypes.cpp MEX file
