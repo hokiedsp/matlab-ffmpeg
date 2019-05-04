92a4a3076ba629967d231869efea0349f72c71d3
Takeshi Ikuma <tikuma@gmail.com>
2019-05-03 20:43:01 -0500

    Initial commit

---
+++ ffmpegget.m
@@ -0,0 +1,15 @@
+function value = ffmpegget(name, name)
+%FFMPEGGET   Retrieves a property value of the media file
+%   FFMPEGGET(FILE, NAME) returns the value of the property
+%   specified by the NAME string.
+%
+%   [Value1,Value2,...] = FFMPEGGET(FILE,Name1,Name2,...) returns multiple 
+%   properties at once.
+%
+%   See Also:
+
+% Copyright 2019 Takeshi Ikuma
+% History:
+% rev. - : (05-03-2019) original release
+
+% Documentation m-file for ffmpeggetprop.cpp MEX file
