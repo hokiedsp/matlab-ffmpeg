bdf52dba59d221e1eb14b1526f9e199a540a1c22
Takeshi Ikuma <tikuma@gmail.com>
2019-05-05 13:05:02 -0500

    Updated ffmpegcolors

---
+++ ffmpegcolors.cpp
@@ -0,0 +1,88 @@
+#include <map>
+#include <string>
+#include <algorithm>
+
+#include <mex.h>
+
+extern "C"
+{
+#include <libavutil/parseutils.h>
+}
+
+#include "ffmpeg/avexception.h"
+#include "ffmpeg/mxutils.h"
+
+// function colors = ffmpegcolors()
+// function colors = ffmpegcolors("onlynames")
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    if (nlhs > 1 || nrhs > 1)
+        mexErrMsgTxt("Takes no input argument and produces 1 output.");
+
+    // parse the input arguments if given
+    std::vector<std::string> options;
+    bool onlynames = false;
+    if (nrhs > 0)
+    {
+        options = mxParseStringArgs(nrhs, prhs, 1, true);
+        auto it = std::find(std::begin(options), std::end(options), "onlynames");
+        if (it != options.end())
+            onlynames = true;
+        if (options.size() > (size_t)onlynames)
+            mexErrMsgTxt("Only input argument supported is \"onlynames\".");
+    }
+
+    // initialize AVException to capture fatal error in ffmpeg
+    AVException::initialize();
+
+    // gather color data
+    std::map<std::string, const uint8_t *> color_map;
+    int index;
+    const char *name;
+    const uint8_t *rgb;
+    for (index = 0; (name = av_get_known_color_name(index, &rgb)); ++index)
+    {
+        std::string name_str(name);
+        std::transform(name_str.begin(), name_str.end(), name_str.begin(), ::tolower); // lower case
+        color_map.insert(std::make_pair(std::move(name_str), rgb));
+    }
+
+    mxArray *mxInfo;
+    if (onlynames)
+    {
+        // return cellstr
+        mxInfo = mxCreateCellMatrix(color_map.size(), 1);
+        index = 0;
+        for (auto it = color_map.begin(); it != color_map.end(); ++it, ++index)
+            mxSetCell(mxInfo, index, mxCreateString(it->first.c_str()));
+    }
+    else
+    { // create struct array
+        mxInfo = mxCreateStructMatrix(1, 1, 0, nullptr);
+
+        index = 0;
+        for (auto it = color_map.begin(); it != color_map.end(); ++it, ++index)
+        {
+            mxArray *mxRGB = mxCreateDoubleMatrix(1, 3, mxREAL);
+            double *rgb_ptr = mxGetPr(mxRGB);
+            rgb_ptr[0] = it->second[0] / 255.0l;
+            rgb_ptr[1] = it->second[1] / 255.0l;
+            rgb_ptr[2] = it->second[2] / 255.0l;
+
+            const char *fname = it->first.c_str();
+            mxAddField(mxInfo, fname);
+            mxSetField(mxInfo, 0, fname, mxRGB);
+        }
+    }
+
+    // if no output requested, display table
+    if (nlhs || onlynames)
+    {
+        plhs[0] = mxInfo;
+    }
+    else
+    {
+        mexCallMATLAB(0, nullptr, 1, &mxInfo, "disp");
+        mxDestroyArray(mxInfo);
+    }
+}
--- ffmpegcolors.m
+++ ffmpegcolors.m
@@ -3,36 +3,12 @@ function info = ffmpegcolors()
 %   FFMPEGCOLORS displays 
 %   COLORS = FFMPEGCOLORS returns the names and RGB values of the
 %   predefined colors in FFMPEG.
+%
+%   COLORS = FFMPEGCOLORS('onlynames') returns only the names.
 
-% Copyright 2015 Takeshi Ikuma
+% Copyright 2015-2019 Takeshi Ikuma
 % History:
 % rev. - : (04-06-2015) original release
-
-narginchk(0,0);
-
-% run FFmpeg
-[s,msg] = system([ffmpegpath() ' -colors']);
-
-if s==0 && isempty(msg)
-   error('ffmpegpixfmts failed to run FFmpeg\n\n%s',msg);
-end
-
-I = regexp(msg,'name\s+#RRGGBB','start','once');
-if isempty(I)
-   error('Incompatible FFmpeg version.');
-end
-msg(1:I-1) = [];
-
-if nargout<1 % no output argument, just display the ffmpeg output
-   disp(msg);
-   return;
-end
-
-% skip the preamble
-msg(1:regexp(msg,'name                             #RRGGBB\n','end','once')) = [];
-
-% scan each line
-info = regexp(msg,'(?<name>\S+)\s+\#(?<rgb>[a-z0-9]{6})\n','names');
-
-val = arrayfun(@(el)hex2dec({el.rgb([1 2]) el.rgb([3 4]) el.rgb([5 6])})'/255,info,'UniformOutput',false);
-[info.rgb] = deal(val{:});
+% rev. 1 : (05-05-2019) 
+%          - Converted to MEX implementation
+%          - Added 'onlynames' option
