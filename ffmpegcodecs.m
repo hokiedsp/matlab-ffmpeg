22dd2faefe1b57c544c796903d31b907c29b941a
Takeshi Ikuma <tikuma@gmail.com>
2019-05-04 19:29:44 -0500

    Rev. 1: major update

--- ffmpegcodecs.m
+++ ffmpegcodecs.m
@@ -1,57 +1,71 @@
-function info = ffmpegcodecs()
+function info = ffmpegcodecs(varargin)
 %FFMPEGCODECS   Retrieves FFMpeg supported codecs
 %   FFMPEGCODECS() displays all the supported codecs.
 %
-%   See Also: FFMPEGSETUP, FFMPEGTRANSCODE
+%   T = FFMPEGCODECS() returns table T listing the supported
+%   codecs. If Matlab version does not support table, a struct array 
+%   is returned instead.
+%
+%   [...] = FFMPEGCODECS('encoders') or [...] = FFMPEGCODECS('decoders') 
+%   limits the output to include only encoders or decoders. Setting this 
+%   option returns additional output fields.
+%
+%   S = FFMPEGCODECS(...,'video'), S = FFMPEGCODECS(...,'audio'),
+%   S = FFMPEGCODECS(...,'subtitle'), or S = FFMPEGCODECS(...,'other') limists 
+%   the output to include only the specified media types. This option is only
+%   available when returning a struct output.
+%
+%   See Also: FFMPEGSETUP, FFMPEGTRANSCODE, FFMPEGFORMATS
 
-% Copyright 2015 Takeshi Ikuma
+% Copyright 2015-2019 Takeshi Ikuma
 % History:
 % rev. - : (04-06-2015) original release
+% rev. 1 : (05-04-2019) Major update:
+%          - Additional input arguments to limit output listing
+%          - Backend MEX implementation if struct is requested
+%          - Returns table rather than struct
 
-narginchk(0,0);
+nargoutchk(0,1);
+
+if nargin>0
+    options = {'encoders','decoders','video','audio','subtitle','other'};
+    varargin(:) = cellfun(@(c)validatestring(c,options),varargin,'UniformOutput',false);
+    tf = ismember(options, varargin);
+    if ~any(tf([1 2]))
+        tf([1 2]) = true;
+    end
+    if ~any(tf(3:end))
+        tf(3:end) = true;
+    end
+else
+    tf = true(1,6);
+end
 
 % run FFmpeg
-[s,msg] = system([ffmpegpath() ' -codecs']);
+if nargout==0
+    if ~all(tf([3:end]))
+        warning('Direct FFmpeg output can only list all media types.');
+    end
 
-if s==0 && isempty(msg)
-   error('ffmpegpixfmts failed to run FFmpeg\n\n%s',msg);
+    if nargin==0 || all(tf([1 2]))
+        [~,msg] = system([ffmpegpath() ' -codecs']);
+    elseif tf(1)
+        [~,msg] = system([ffmpegpath() ' -encoders']);
+    else
+        [~,msg] = system([ffmpegpath() ' -decoders']);
+    end
+
+    I = regexp(msg,'Codecs|Encoders|Decoders','once');
+
+    disp(msg(I:end));
+else
+    % call private mex function to retrieve the info struct
+    info = ffmpegcodecs_mex(tf(1),tf(2),tf(3),tf(4),tf(5),tf(6));
+
+    % attempt to return the results in table format
+    try
+        info = struct2table(info);
+    catch
+        
+    end
 end
-
-I = regexp(msg,'Codecs:','start','once');
-if isempty(I)
-   error('Incompatible FFmpeg version.');
-end
-msg(1:I-1) = [];
-
-if nargout<1 % no output argument, just display the ffmpeg output
-   disp(msg);
-   return;
-end
-
-% skip the preamble
-msg(1:regexp(msg,'-------\n','end','once')) = [];
-
-% scan each line
-info = regexp(msg,'(?<decoder>[D\.])(?<encoder>[E\.])(?<type>[VAS])(?<intraframe>[I\.])(?<lossy>[L\.])(?<lossless>[S\.])\s+(?<name>\S+)\s+(?<description>[^\n]+)\n','names');
-
-% convert flags to logical
-val = arrayfun(@(el)el.decoder=='D',info,'UniformOutput',false);
-[info.decoder] = deal(val{:});
-
-val = arrayfun(@(el)el.encoder=='E',info,'UniformOutput',false);
-[info.encoder] = deal(val{:});
-
-val = arrayfun(@(el)validatestring(el.type,{'video','audio','subtitle'}),info,'UniformOutput',false);
-[info.type] = deal(val{:});
-
-val = arrayfun(@(el)el.intraframe=='I',info,'UniformOutput',false);
-[info.intraframe] = deal(val{:});
-
-val = arrayfun(@(el)el.lossy=='L',info,'UniformOutput',false);
-[info.lossy] = deal(val{:});
-
-val = arrayfun(@(el)el.lossless=='S',info,'UniformOutput',false);
-[info.lossless] = deal(val{:});
-
-% reorder the fields so they are presented in more logical way
-info = orderfields(info,[7 3 8 1 2 4 5 6]);
