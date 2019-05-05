881b01ed5b7454ef0ea23d6aa18e0181c100000a
Takeshi Ikuma <tikuma@gmail.com>
2019-05-04 19:28:57 -0500

    Output table instead of struct array

--- ffmpegformats.m
+++ ffmpegformats.m
@@ -1,48 +1,67 @@
-function info = ffmpegformats()
+function info = ffmpegformats(varargin)
 %FFMPEGFORMATS   Retrieves FFMpeg supported formats
-%   FFMPEGCODECS() displays all the supported formats.
+%   FFMPEGFORMATS() displays all the supported formats.
 %
-%   S = FFMPEGCODECS() returns struct array S containing the supported
-%   formats. The sturct fields are: name, description, demux, and mux
+%   T = FFMPEGFORMATS() returns table T listing the supported
+%   formats. If Matlab version does not support table, a struct array 
+%   is returned instead.
+%
+%   [...] = FFMPEGFORMATS('muxers') or [...] = FFMPEGFORMATS('demuxers') 
+%   limits the output to include only muxers or demuxers.
+%
+%   [...] = FFMPEGFORMATS(...,'devices') limists the output to include only
+%   the streaming devices. While FFMPEGFORMATS can only display all devices 
+%   (no output argument), its struct output could be limited to only contain
+%   mux or demux devices.
 %
 %   See Also: FFMPEGSETUP, FFMPEGTRANSCODE
 
-% Copyright 2015 Takeshi Ikuma
+% Copyright 2015-2019 Takeshi Ikuma
 % History:
 % rev. - : (04-06-2015) original release
+% rev. 1 : (05-04-2019) Major update:
+%          - Additional input arguments to limit output listing
+%          - Backend MEX implementation if struct is requested
+%          - Returns table rather than struct
 
-narginchk(0,0);
+narginchk(0,2);
+nargoutchk(0,1);
+
+if nargin>0
+    varargin(:) = cellfun(@(c)validatestring(c,{'muxers','demuxers','devices'}),varargin,'UniformOutput',false);
+    tf = ismember({'muxers','demuxers','devices'}, varargin);
+end
 
 % run FFmpeg
-[s,msg] = system([ffmpegpath() ' -formats']);
+if nargout==0
+    % ffmpeg can only show all formats, all muxers, all demuxers, or all devices
+    if nargin==0 || all(tf==[true true false])
+        [~,msg] = system([ffmpegpath() ' -formats']);
+    elseif tf(3)
+        if sum(tf)>1
+            warning('Direct FFmpeg output can only show all (i.e., both mux and demux) devices.');
+        end
+        [~,msg] = system([ffmpegpath() ' -devices']);
+    elseif tf(1)
+        [~,msg] = system([ffmpegpath() ' -muxers']);
+    else
+        [~,msg] = system([ffmpegpath() ' -demuxers']);
+    end
 
-if s==0 && isempty(msg)
-   error('ffmpegpixfmts failed to run FFmpeg\n\n%s',msg);
+    I = regexp(msg,'(?:File|Devices).+','once');
+
+    disp(msg(I:end));
+else
+    % call private mex function to retrieve the info struct
+    if nargin==0
+        tf = false(1,3);
+    end
+    info = ffmpegformats_mex(tf(1),tf(2),tf(3));
+    
+    % attempt to return the results in table format
+    try
+        info = struct2table(info);
+    catch
+        
+    end
 end
-
-I = regexp(msg,'File formats:','start','once');
-if isempty(I)
-   error('Incompatible FFmpeg version.');
-end
-msg(1:I-1) = [];
-
-if nargout<1 % no output argument, just display the ffmpeg output
-   disp(msg);
-   return;
-end
-
-% skip the preamble
-msg(1:regexp(msg,'--\n','end','once')) = [];
-
-% scan each line
-info = regexp(msg,'(?<demux>[D\ ])(?<mux>[E\ ])\s+(?<name>\S+)\s+(?<description>[^\n]+)\n','names');
-
-% convert flags to logical
-val = arrayfun(@(el)el.demux=='D',info,'UniformOutput',false);
-[info.demux] = deal(val{:});
-
-val = arrayfun(@(el)el.mux=='E',info,'UniformOutput',false);
-[info.mux] = deal(val{:});
-
-% reorder the fields so they are presented in more logical way
-info = orderfields(info,[3 4 1 2]);
