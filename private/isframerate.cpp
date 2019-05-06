f5123eac840975099f391697fd046c5ea60e776c
Takeshi Ikuma <tikuma@gmail.com>
2019-05-05 19:42:17 -0500

    MEXified iframerate()

---
+++ private/isframerate.cpp
@@ -0,0 +1,38 @@
+#include <memory>
+#include <algorithm> // transform()
+#include <map>
+#include <string>
+
+#include <mex.h>
+
+extern "C"
+{
+#include <libavutil/parseutils.h>
+}
+
+#include "ffmpeg/avexception.h"
+#include "ffmpeg/mxutils.h"
+
+// tf = isframerate(val) (prevalidated)
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    std::string expr;
+    if (mxIsNumeric(prhs[0]))
+    {
+        // convert to string
+        mxArray *mxStr;
+        mexCallMATLAB(1, &mxStr, 1, (mxArray**)prhs, "num2str");
+        expr = mxArrayToStdString(mxStr);
+        mxDestroyArray(mxStr);
+    }
+    else
+    {
+        expr = mxArrayToStdString(prhs[0]);
+    }
+
+    // initialize AVException
+    AVException::initialize();
+
+    AVRational rate; // dummy
+    plhs[0] = mxCreateLogicalScalar(av_parse_video_rate(&rate, expr.c_str())>=0);
+}
