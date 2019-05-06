4a0f2a0c2f2b99271949dd892c658a6c552410db
Takeshi Ikuma <tikuma@gmail.com>
2019-05-05 20:06:52 -0500

    MEXified ispixfmt()

---
+++ private/ispixfmt.cpp
@@ -0,0 +1,26 @@
+#include <memory>
+#include <algorithm> // transform()
+#include <map>
+#include <string>
+
+#include <mex.h>
+
+extern "C"
+{
+#include <libavutil/pixdesc.h>
+}
+
+#include "ffmpeg/avexception.h"
+#include "ffmpeg/mxutils.h"
+
+// tf = ispixfmt(val) (prevalidated)
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+    std::string expr = mxArrayToStdString(prhs[0]);
+
+    // initialize AVException
+    AVException::initialize();
+
+    // try to convert string to av_get_pix_fmt
+    plhs[0] = mxCreateLogicalScalar(av_get_pix_fmt(expr.c_str()) != AV_PIX_FMT_NONE);
+}
